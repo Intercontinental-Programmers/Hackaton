0fa4706221af268ef92a3f2d7d510fd09e9eb9d9
Maciej Hajduk <ebrithil97@gmail.com>
2017-06-03 13:47:17 +0200

    we ahve it

--- src/main/java/food/track/LoginController.java
+++ src/main/java/food/track/LoginController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LoginController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String redirect() {
-        System.out.println("im logging in");
+        //System.out.println("im logging in");
         Day day1 = new Day(1,1);
         Day day2 = new Day(2,1);
         User usr1 = new User(1);
         User usr2 = new User(2);
-        System.out.println(day1+"\n\n"+day2);
-        System.out.println(usr1+"\n\n"+usr2);
+        //System.out.println(day1+"\n\n"+day2);
+        //System.out.println(usr1+"\n\n"+usr2);
         return "login";
     }
 }
