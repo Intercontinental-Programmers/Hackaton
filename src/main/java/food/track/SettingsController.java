f32afb9de4a128f56d9a92dd86e2bd81bfb2e2f6
Maciej Hajduk <ebrithil97@gmail.com>
2017-06-03 14:27:04 +0200

    i forgot

--- src/main/java/food/track/SettingsController.java
+++ src/main/java/food/track/SettingsController.java
@@ -1,7 +1,15 @@
 package food.track;
 
-/**
- * Created by okkindel on 03.06.17.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
 public class SettingsController {
+    @RequestMapping(value = "/settings", method = RequestMethod.GET)
+    public String redirect() {
+        System.out.println("im loading settings");
+        return "settings";
+    }
 }
