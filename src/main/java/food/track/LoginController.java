b288e977c8971df1c96bc6ba39aa14a7fa8b5cd6
Adrian <kestarumper8@gmail.com>
2017-06-03 12:50:39 +0200

    user loaded from DB

--- src/main/java/food/track/LoginController.java
+++ src/main/java/food/track/LoginController.java
@@ -12,7 +12,10 @@ public class LoginController {
         System.out.println("im logging in");
         Day day1 = new Day(1,1);
         Day day2 = new Day(2,1);
+        User usr1 = new User(1);
+        User usr2 = new User(2);
         System.out.println(day1+"\n\n"+day2);
+        System.out.println(usr1+"\n\n"+usr2);
         return "login";
     }
 }
--- src/main/java/food/track/User.java
+++ src/main/java/food/track/User.java
@@ -1,20 +1,59 @@
 package food.track;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class User {
     private String nick;
-    private String login;
-    private String password;
     private String email;
+    private String age;
+    private String height;
+    private String weight;
+    private String delta;
 
-    public User () {}
+    public User (int id) {
+        String connectionUrl = "jdbc:sqlserver://foodtrack.database.windows.net:1433;database=FoodTrack;user=notroot@foodtrack;password=adihajmaktre+69";
+
+        // Declare the JDBC objects.
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            // Establish the connection.
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection(connectionUrl);
+
+            // Create and execute an SQL statement that returns some data.
+            String SQL = "SELECT nick, email, age, height, weight, delta FROM users WHERE id_users = "+id;
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(SQL);
+
+            // Iterate through the data in the result set and display it.
+            rs.next();
+            nick = rs.getString(1);
+            email = rs.getString(2);
+            age = rs.getString(3);
+            height = rs.getString(4);
+            weight = rs.getString(5);
+            delta = rs.getString(6);
+        }
+
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (rs != null) try { rs.close(); } catch(Exception e) {}
+            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
+            if (con != null) try { con.close(); } catch(Exception e) {}
+        }
+    }
+
     public String getNick(){
         return nick;
-    }
-    public String getLogin(){
-        return login;
-    }
-    public String getPassword(){
-        return password;
     }
     public String getEmail(){
         return email;
@@ -24,13 +63,51 @@ public class User {
     public void setNick(String nick){
         this.nick = nick;
     }
-    public void setLogin(String login){
-        this.login = login;
-    }
-    public void setPassword(String password){
-        this.password = password;
-    }
     public void setEmail(String email){
         this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getDelta() {
+        return delta;
+    }
+
+    public void setDelta(String delta) {
+        this.delta = delta;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "nick='" + nick + '\'' +
+                ", email='" + email + '\'' +
+                ", age='" + age + '\'' +
+                ", height='" + height + '\'' +
+                ", weight='" + weight + '\'' +
+                ", delta='" + delta + '\'' +
+                '}';
     }
 }
